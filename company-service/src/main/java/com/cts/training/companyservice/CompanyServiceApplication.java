00de227d8bd788645ec874a9ba8204d0143e5e44
Syed Ehsan Ali Razvi <ehsan.razvi92@gmail.com>
2020-03-14 08:23:59 +0530

    push

--- company-service/src/main/java/com/cts/training/companyservice/CompanyServiceApplication.java
+++ company-service/src/main/java/com/cts/training/companyservice/CompanyServiceApplication.java
@@ -3,13 +3,19 @@ package com.cts.training.companyservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.cts.training.companyservice.repo.CompanyRepo;
 
 @SpringBootApplication(scanBasePackages = "com.cts.training.companyservice")
 @EnableDiscoveryClient
 public class CompanyServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CompanyServiceApplication.class, args);
+//		SpringApplication.run(CompanyServiceApplication.class, args);
+		ConfigurableApplicationContext c=SpringApplication.run(CompanyServiceApplication.class, args);
+		CompanyRepo companyRepoBean=c.getBean(CompanyRepo.class);
+		System.out.println(companyRepoBean.findListedInByName("APPLE"));
 	}
 
 }
--- company-service/src/main/java/com/cts/training/companyservice/repo/CompanyRepo.java
+++ company-service/src/main/java/com/cts/training/companyservice/repo/CompanyRepo.java
@@ -6,11 +6,14 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cts.training.companyservice.entity.Company;
+import com.cts.training.companyservice.entity.ListedIn;
 
 public interface CompanyRepo extends JpaRepository<Company, Integer>{
 	
 	public List<Company> findAllByNameContaining(String pattern);
 	
 	public Optional<Company> findByName(String name);
+	
+	public List<ListedIn> findListedInByName(String companyName);
 
 }
